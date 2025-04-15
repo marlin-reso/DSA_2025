5690496cff9f05fcea2bcfc72d116534d4d5ea3b
MARLIN <sanju.reso@gmail.com>
2025-04-15 07:32:28 +0530

    basic_01

---
+++ src/logicBuildingProblem/ReverseDigit.java
@@ -0,0 +1,19 @@
+package logicBuildingProblem;
+
+public class ReverseDigit {
+
+	public static void main(String[] args) {
+		 int digit = 456;
+		 int reverse = 0;
+		 
+		 for(int i=0; i<=2; i++) {
+			 
+			 reverse = digit%10+reverse *10;
+			 digit = digit/10;
+		 }
+		 
+		 System.out.println(reverse);
+
+	}
+
+}
